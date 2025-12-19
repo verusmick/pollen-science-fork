350a303290cc678d2369212234a74602a6b02327
verusmick <miguel.angel.angulo72@gmail.com>
2025-12-19 13:27:39 -0400

    Update apiBase URL in production configuration

--- webpack.prod.js
+++ webpack.prod.js
@@ -11,7 +11,7 @@ module.exports = merge(common, {
     plugins: [
         new webpack.DefinePlugin({
             'global.env': {
-                apiBase: JSON.stringify(''),
+                apiBase: JSON.stringify('https://pollenscience.eu'),
             },
         }),
     ]
