f6eff674ad35e035e4c7ee8da221b80bd669893a
Miguel Angel <miguel.angel.angulo72@gmail.com>
2026-01-14 15:41:04 -0400

    i've created search and select components

---
+++ src/components/ui/searchInput/SearchInput.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="input-group search-input">
+    <input type="text" class="form-control" :placeholder="placeholder" :value="modelValue" @input="onInput"
+      @keyup.enter="emitSearch" />
+
+    <div class="input-group-append">
+      <button class="btn btn-outline-secondary" type="button" @click="emitSearch" aria-label="Search">
+        <span class="fad fa-search"></span>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  modelValue: {
+    type: String,
+    default: ''
+  },
+  placeholder: {
+    type: String,
+    default: 'Search...'
+  }
+})
+
+const emit = defineEmits(['update:modelValue', 'search'])
+
+function onInput(event) {
+  emit('update:modelValue', event.target.value)
+}
+
+function emitSearch() {
+  emit('search')
+}
+</script>
+
+<style scoped>
+.search-input .form-control {
+  border-right: 0;
+}
+
+.search-input .btn {
+  border-left: 0;
+}
+</style>
