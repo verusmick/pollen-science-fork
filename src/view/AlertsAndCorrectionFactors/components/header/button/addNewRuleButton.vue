7c1a4da5a60b957c978a1bfe2ae2c27ee86f32f4
Miguel Angel <miguel.angel.angulo72@gmail.com>
2026-01-19 18:32:53 -0400

    Adding Alerts and correction factors list

---
+++ src/view/AlertsAndCorrectionFactors/components/header/button/addNewRuleButton.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { AkCirclePlusFill } from '@kalimahapps/vue-icons';
+</script>
+
+<template>
+  <button class=" btn btn-lg btn-primary icon-btn add-new-rule-button">
+    <AkCirclePlusFill />
+    <p v-t="'page.rulesAndNotifications.addNewRule'" />
+  </button>
+
+</template>
+
+<style scoped>
+.add-new-rule-button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 54px;
+  width: 176px;
+
+  p {
+    margin-left: 5px;
+    font-size: 16px;
+    font-weight: 300;
+    margin-bottom: 0;
+  }
+}
+</style>
