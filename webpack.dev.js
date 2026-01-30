bbe2dbb6706a57ae2b2d7d9e1417890fa2c3cd5f
Miguel Angel <miguel.angel.angulo72@gmail.com>
2026-01-30 11:47:33 -0400

    Changing api path to staging env

--- webpack.dev.js
+++ webpack.dev.js
@@ -16,7 +16,7 @@ module.exports = merge(common, {
     plugins: [
         new webpack.DefinePlugin({
             'global.env': {
-                apiBase: JSON.stringify('https://pollenscience.eu'),
+                apiBase: JSON.stringify('http://staging.pollen.enjambre.com.bo:8080'),
             },
         }),
     ],
